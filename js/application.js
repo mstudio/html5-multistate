acdb15fad08b48abb51d92bc4db94c33563a4bcb
Alex Motzenbecker <alex@mstudio.com>
2011-11-30 00:12:50 -0500

    first update

---
+++ js/application.js
@@ -0,0 +1,51 @@
+/***
+ * @author  rimtap
+ * @description eat shit
+ */
+
+
+my_application = function(){
+    /**** private jQuery DOM vars ***/
+    var $sample;
+    
+     /*** private application vars ***/
+    var sample;
+        
+    return{
+        /*** public vars ***/
+     
+        /*** public methods ***/
+        init:function(){
+            $sample = $("#sample");
+	}
+    }
+    
+    /*** private methods ***/
+    function sampleMethod(){
+        
+    }
+    
+
+    /*
+     *
+     * Utilities
+     * 
+     */
+    function log(msg){
+        if (typeof console != 'undefined'){ // prevents IE error
+            console.log(msg);
+        }
+    }
+    
+    function shuffle(o){
+        for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+}();
+
+
+
+
+$(document).ready(function(){
+     my_application.init();
+});
